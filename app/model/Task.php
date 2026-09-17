233f2938b94c706419fbf504f4186f518903c6a8
sinanyzl <sinanyozgatli01@gmail.com>
2026-09-17 14:33:45 +0300

    today

--- app/model/Task.php
+++ app/model/Task.php
@@ -1,65 +1,69 @@
-<?php 
+<?php
 
 
-function insert_task($conn, $data){
+function insert_task($conn, $data)
+{
 
-$sql = "INSERT INTO tasks (title, description, assigned_to, due_to) VALUES (?,?,?,?)";
-$stmt = $conn->prepare($sql);
-$stmt->execute($data);
-
+    $sql = "INSERT INTO tasks (title, description, assigned_to, due_to) VALUES (?,?,?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($data);
 }
 
-function get_all_tasks($conn){
+function get_all_tasks($conn)
+{
 
     $sql = "SELECT * FROM tasks ORDER BY id DESC";
     $stmt = $conn->prepare($sql);
     $stmt->execute([]);
 
-    if($stmt->rowCount() > 0){
+    if ($stmt->rowCount() > 0) {
         $tasks = $stmt->fetchAll();
-
-    }else $tasks = 0;
+    } else $tasks = 0;
 
     return $tasks;
 }
 
-function get_all_tasks_due_today ($conn){
-    
-$sql = "SELECT * FROM tasks WHERE due_date = CURDATE() AND status != 'complated' ORDER BY id DESC ";
-$stmt = $conn->prepare($sql);
-$stmt->execute([]);
+function get_all_tasks_due_today($conn)
+{
 
-if($stmt->rowCount() > 0){
-    $tasks = $stmt->fetchALL();
-    
-}else $tasks = 0;
+    $sql = "SELECT * FROM tasks WHERE due_date = CURDATE() AND status != 'complated' ORDER BY id DESC ";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([]);
 
-return $tasks;
+    if ($stmt->rowCount() > 0) {
+        $tasks = $stmt->fetchALL();
+    } else $tasks = 0;
 
+    return $tasks;
 }
 
-function count_tasks_due_today($conn){
+function count_tasks_due_today($conn)
+{
     $sql = "SELECT id FROM tasks WHERE due_date = CURDATE() AND status != 'completed'";
     $stmt = $conn->prepare($sql);
     $stmt->execute([]);
 
     return $stmt->rowCount();
-
 }
 
-function get_all_tasks_overdue($conn){
+function get_all_tasks_overdue($conn)
+{
 
-$sql = "SELECT * FROM tasks WHERE due_date < CURDATE() AND status != 'completed' ORDER BY id DESC";
-$stmt = $conn->prepare($sql);
-$stmt->execute([]);
+    $sql = "SELECT * FROM tasks WHERE due_date < CURDATE() AND status != 'completed' ORDER BY id DESC";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([]);
 
-if($stmt->rowCount > 0){
-    $tasks = $stmt->fetchAll();
-    
-}else $tasks = 0;
-return $tasks;
-
+    if ($stmt->rowCount > 0) {
+        $tasks = $stmt->fetchAll();
+    } else $tasks = 0;
+    return $tasks;
 }
 
+function count_tasks_overdue($conn)
+{
+    $sql = "SELECT id FROM tasks WHERE due_date < CURDATE() AND status != 'completed'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([]);
 
-?>
+    return $stmt->rowCount();
+}
