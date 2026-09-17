c01fc68a7d571d9b0116ba380eceffc8d1378f5b
sinanyzl <sinanyozgatli01@gmail.com>
2026-09-17 14:12:13 +0300

    today

--- app/model/Task.php
+++ app/model/Task.php
@@ -1,5 +1,15 @@
 <?php 
 
 
+function insert_task($conn, $data){
+
+$sql = "INSERT INTO tasks (title, description, assigned_to, due_to) VALUES (?,?,?,?)";
+$stmt = $conn->prepare($sql);
+$stmt->execute($data);
+
+}
+
+
+
 
 ?>
