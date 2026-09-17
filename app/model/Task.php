cc04e0f8b45ac919cb9fd34fa55908d03b0660bf
sinanyzl <sinanyozgatli01@gmail.com>
2026-09-17 14:28:48 +0300

    today

--- app/model/Task.php
+++ app/model/Task.php
@@ -23,6 +23,43 @@ function get_all_tasks($conn){
     return $tasks;
 }
 
+function get_all_tasks_due_today ($conn){
+    
+$sql = "SELECT * FROM tasks WHERE due_date = CURDATE() AND status != 'complated' ORDER BY id DESC ";
+$stmt = $conn->prepare($sql);
+$stmt->execute([]);
+
+if($stmt->rowCount() > 0){
+    $tasks = $stmt->fetchALL();
+    
+}else $tasks = 0;
+
+return $tasks;
+
+}
+
+function count_tasks_due_today($conn){
+    $sql = "SELECT id FROM tasks WHERE due_date = CURDATE() AND status != 'completed'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([]);
+
+    return $stmt->rowCount();
+
+}
+
+function get_all_tasks_overdue($conn){
+
+$sql = "SELECT * FROM tasks WHERE due_date < CURDATE() AND status != 'completed' ORDER BY id DESC";
+$stmt = $conn->prepare($sql);
+$stmt->execute([]);
+
+if($stmt->rowCount > 0){
+    $tasks = $stmt->fetchAll();
+    
+}else $tasks = 0;
+return $tasks;
+
+}
 
 
 ?>
