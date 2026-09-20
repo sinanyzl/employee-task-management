0e7a074d040e37b0296a88973cba95c47ff547db
sinanyzl <sinanyozgatli01@gmail.com>
2026-09-20 04:56:08 +0300

    today

--- app/model/Task.php
+++ app/model/Task.php
@@ -139,15 +139,15 @@ function update_task_status($conn, $data)
     $stmt->execute($data);
 }
 
-function get_all_tasks_by_id($conn, $id){
+function get_all_tasks_by_id($conn, $id)
+{
     $sql = "SELECT * FROM tasks WHERE assigned_to=?";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$id]);
 
     if ($stmt->rowCount() > 0) {
         $tasks = $stmt->fetchAll();
-
-    }else $tasks = 0;
+    } else $tasks = 0;
 
     return $tasks;
 }
@@ -162,3 +162,74 @@ function count_pending_tasks($conn)
     return $stmt->rowCount();
 }
 
+function count_in_progress_tasks($conn)
+{
+    $sql = "SELECT id FROM tasks WHERE status = 'in_progress'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([]);
+
+    return $stmt->rowCount();
+}
+
+function count_completed_tasks($conn)
+{
+    $sql = "SELECT id FROM tasks WHERE status = 'completed'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([]);
+
+    return $stmt->rowCount();
+}
+
+function count_my_tasks($conn, $id)
+{
+    $sql = "SELECT id FROM tasks WHERE assigned_to=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+
+    return $stmt->rowCount();
+}
+
+function count_my_tasks_overdue($conn, $id)
+{
+    $sql = "SELECT id FROM tasks WHERE due_date < CURDATE() AND status != 'completed' AND assigned_to=? AND due_date != '0000-00-00'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+
+    return $stmt->rowCount();
+}
+
+function count_my_tasks_NoDeadline($conn, $id)
+{
+    $sql = "SELECT id FROM tasks WHERE assigned_to=? AND status != 'completed' AND due_date IS NULL OR due_date != '0000-00-00'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+
+    return $stmt->rowCount();
+}
+
+function count_my_pending_tasks($conn, $id)
+{
+    $sql = "SELECT id FROM tasks WHERE status = 'pending' AND assigned_to=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+
+    return $stmt->rowCount();
+}
+
+function count_my_in_progress_tasks($conn, $id)
+{
+    $sql = "SELECT id FROM tasks WHERE status = 'in_progress' AND assigned_to=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+
+    return $stmt->rowCount();
+}
+
+function count_my_completed_tasks($conn, $id)
+{
+    $sql = "SELECT id FROM tasks WHERE status = 'completed' AND assigned_to=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$id]);
+
+    return $stmt->rowCount();
+}
