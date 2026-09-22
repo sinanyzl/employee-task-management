518d02301571c52542440bafbac66303b3c576b5
sinanyzl <sinanyozgatli01@gmail.com>
2026-09-22 20:19:49 +0300

    today

--- app/model/User.php
+++ app/model/User.php
@@ -19,3 +19,9 @@ function insert_user($conn, $data)
     $stmt = $conn->prepare($sql);
     $stmt->execute($data);
 }
+function update_user($conn, $data)
+{
+    $sql = "UPDATE users SET full_name=?, username=?, password=?, role=? WHERE id=? AND role=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($data);
+}
