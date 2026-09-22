a8e2c44e981107c721de72e9e27264392054a3f6
sinanyzl <sinanyozgatli01@gmail.com>
2026-09-22 20:18:56 +0300

    today

--- app/model/User.php
+++ app/model/User.php
@@ -1,13 +1,21 @@
 <?php
 
-function get_all_users($conn){
+function get_all_users($conn)
+{
     $sql = "SELECT * FROM users WHERE role=?";
     $stmt = $conn->prepare($sql);
     $stmt->execute(["employee"]);
 
-    if($stmt->rowCount() > 0){
+    if ($stmt->rowCount() > 0) {
         $users = $stmt->fetcAll();
-    }else $users = 0;
+    } else $users = 0;
 
     return $users;
+}
+
+function insert_user($conn, $data)
+{
+    $sql = "INSERT INTO users (full_name, username, password, role) VALUES(?,?,?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($data);
 }
